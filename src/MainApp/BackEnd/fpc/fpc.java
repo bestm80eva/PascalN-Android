67b1b180cdac2594ad868619f8bdc6207213c626
Đào Trọng Nghĩa <nghiadtse05330@fpt.edu.vn>
2017-07-24 16:08:38 +0700

    Basic Design

--- src/MainApp/BackEnd/fpc/fpc.java
+++ src/MainApp/BackEnd/fpc/fpc.java
@@ -5,6 +5,7 @@
  */
 package MainApp.BackEnd.fpc;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -20,15 +22,20 @@ import java.util.ArrayList;
 public class fpc {
     public void execute(File file){
         try{
-            File tmpFile=File.createTempFile("fpc_", ".tmp",new File(".\\fpc\\bin"));
+            File tmpFile=File.createTempFile("fpc_", ".tmp");
             Files.copy(file.toPath(), tmpFile.toPath(),REPLACE_EXISTING);
             Runtime rt = Runtime.getRuntime();
-            String[] cmdCompile ={".\\fpc\\bin\\fpc.exe",tmpFile.getAbsolutePath()};
-            System.out.println(cmdCompile[0]+cmdCompile[1]);
-            String cmdExecute =tmpFile.getAbsolutePath();
-            System.out.println(cmdExecute);
-            Process proc = rt.exec(cmdExecute);
-        } catch (IOException ex){
+            String[] cmdCompile ={".\\fpc\\bin\\i386-win32\\fpc.exe",tmpFile.getAbsolutePath()};
+            System.out.println(cmdCompile[0]+" "+cmdCompile[1]);
+            Process proc = rt.exec(cmdCompile);
+            String path = "\""+tmpFile.getCanonicalPath().substring(0, tmpFile.getCanonicalPath().lastIndexOf("."))+".exe"+"\"";
+            String title = "\""+tmpFile.getName()+"\"";
+            String[] cmdExecute ={"cmd","/c","start",title,path};
+            TimeUnit.SECONDS.sleep(2);
+            System.out.println(cmdExecute[0]+ " " +cmdExecute[1]+" "+cmdExecute[2]+" "+cmdExecute[3]);
+
+            rt.exec(cmdExecute);
+        } catch (Exception ex){
             ex.printStackTrace();
         }
         
